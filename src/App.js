3d89513338fa7645447a4ad6b9ce3d4a208b3379
Stepan Subrt <stepansubrt@icloud.com>
2022-01-26 21:06:40 +0100

    mapping over data and rendering card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import './App.css';
 import Header from './components/Header';
-
+import Card from './components/Card';
+import data from './data';
+import {nanoid} from 'nanoid';
 function App() {
+  console.log(data);
+  const cards = data.map(card => {
+    return (
+      <Card 
+      key={nanoid()}
+      image={card.imageUrl}
+      location={card.location}
+      googleMaps={card.googleMapsUrl}
+      title={card.title}
+      startDate={card.startDate}
+      endDate={card.endDate}
+      description={card.description}
+      />
+    )
+  })
   return (
     <div className="App">
-      <Header />
+      <header>
+        <Header />
+      </header>
+      <main>
+        {cards}
+      </main>
     </div>
   );
 }
