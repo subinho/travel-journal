cc5d41a42033edd44b600c50738f999b236af4b0
Stepan Subrt <stepansubrt@icloud.com>
2022-01-26 21:07:52 +0100

    adding classes etc

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,22 @@
 import React from 'react';
 
-export default function Card() {
+export default function Card(props) {
   return (
       <div className='card'>
+          <div>
+                <img src={props.image} alt='' className='card-image'/>
+          </div>
+          <div className='card-info'>
+              <div className='card-info-location'>
+                <span className='card-info-location-title'>{props.location}</span>
+                <span className='card-info-google-maps'><a href={props.googleMaps} target='_blank'>View on google maps</a></span>
+              </div>
+              <div className='card-info-description'>
+                <h2>{props.title}</h2>
+                <p><strong>{props.startDate} - {props.endDate}</strong></p>
+                <span>{props.description}</span>
+              </div>
+          </div>
           
       </div>
   );
