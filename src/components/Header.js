bbfb88829bdad46769509fdc69b1d7f45df0e3a0
Stepan Subrt <stepansubrt@icloud.com>
2022-01-26 14:02:30 +0100

    rendering image and title

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import globe from '../images/globe.png'
 export default function Header() {
   return (
       <div className="header">
-          
+          <img src={globe} alt='' className='header-globe'/>
+          <h3 className='header-title'>my travel journal</h3>
       </div>
   );
 }
